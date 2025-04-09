70d96574cdef37850c4f0187880dbb4272906871
zenos <zenos.chen@foxmail.com>
2025-04-09 21:26:12 +0800

    update code

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import { Avatar as AvatarVue, AvatarCircle as AvatarVueCircle } from "./vue/index";
-import { Avatar as AvatarReact, AvatarCircle as AvatarReactCircle } from "./react/index";
-
-export { AvatarVue, AvatarVueCircle, AvatarReactCircle, AvatarReact };
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ["src/index.ts"],
-	format: ["esm", "cjs"],
+	entry: ["src/react/index.ts", "src/vue/index.ts"],
 	outDir: "dist",
+	format: ["esm", "cjs"],
 	dts: true,
 	clean: true,
+	splitting: false,
+	sourcemap: true,
+	skipNodeModulesBundle: true,
+	target: "esnext",
 });
